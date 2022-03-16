8b32be9cb8f7065edf955865b83c54d37a91f3a6
Shubham Kumar <unresolved.shubham@gmail.com>
2022-03-17 00:17:52 +0530

    [NEW] : Landing Page Completed.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
-import Header from './components/header'; 
+import "./App.css";
+import Header from "./Pages/header";
 import { Route, Routes } from "react-router-dom";
-import LandingPage from './components/LandingPage';
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
-    <div style={{display: 'flex', flexDirection: 'column'}}>
-      {/* This header will appear on all the pages */} 
-      <Header/> 
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {/* This header will appear on all the pages */}
+      <Header />
 
       {/* These are the routes. */}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
+      <div
+        className="main-content"
+        style={{ width: "90%", alignSelf: "center",marginTop: '25px' }}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
