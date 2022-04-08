d0802cb514447da06fb08f6df6d9a7c8c96d4a96
Ankit Raj <ankitrajk27@gmail.com>
2022-04-08 09:21:25 +0530

    [NEW] Added a flickity caraousel

--- src/Pages/LandingPage/index.js
+++ src/Pages/LandingPage/index.js
@@ -1,11 +1,17 @@
 import NoticeBoard from "../../Components/NoticeBoard";
+import Flickity from "react-flickity-component";
+import "./landing.css";
+
+const flickityOptions = {
+  initialIndex: 3,
+}
 
 export default function LandingPage() {
   return (
     <div
       style={{
         display: "flex",
-        flexDirection:"row",
+        flexDirection: "column",
         flex: "1",
         width: "100%",
         height: "200%",
@@ -13,13 +19,13 @@ export default function LandingPage() {
       }}
     >
       <div
-      style={{
-        display:"flex",
-        flex:"1",
-        width:"100%",
-        flexDirection:"row",
-        backgroundColor:"red",
-      }}>
+        style={{
+          display: "flex",
+          flex: "1",
+          width: "100%",
+          flexDirection: "row",
+        }}
+      >
         <div
           style={{
             display: "flex",
@@ -28,14 +34,29 @@ export default function LandingPage() {
             position: "relative",
             justifyContent: "center",
             alignItems: "center",
+            paddingTop:"100px",
           }}
         >
-          {/* <div>dfj</div>   */}
-          <img
-            src={"/icons/Iconic Image@4x.png"}
-            alt="Iconic"
-            style={{ display: "flex", width: "500px", height: "500px" }}
-          />
+          {/* FLICKITY */}
+
+          <Flickity
+            className={"carousel"} // default ''
+            elementType={"div"} // default 'div'
+            options={flickityOptions} // takes flickity options {}
+            disableImagesLoaded={false} // default false
+            reloadOnUpdate // default false
+            static // default false
+            data-flickity='{"autoPlay":true}'
+            autoPlay="True"
+            
+          >
+            <img src="/icons/Iconic Image@4x.png" alt="image1" style={{ display: "flex", width: "650px", height: "500px" , alignContent:"center",marginRigh:"20px",marginLeft:"20px",}} />
+            <img src="/icons/Iconic Image@4x.png" alt="image2" style={{ display: "flex", width: "650px", height: "500px" , alignContent:"center",marginRigh:"20px",marginLeft:"20px",}} />
+            <img src="/icons/Iconic Image@4x.png" alt="image3" style={{ display: "flex", width: "650px", height: "500px" , alignContent:"center" ,marginRigh:"20px",marginLeft:"20px", }}/>
+            <img src="/icons/Iconic Image@4x.png" alt="image4" style={{ display: "flex", width: "650px", height: "500px" , alignContent:"center",marginRigh:"20px",marginLeft:"20px",}}/>
+          </Flickity>
+
+          
         </div>
 
         <div
@@ -52,6 +73,15 @@ export default function LandingPage() {
           <NoticeBoard title={"NOTICE BOARD"} />
           <NoticeBoard title={"UPCOMING EVENTS"} />
         </div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flex: "1",
+        }}
+      >
+        dff
       </div>
     </div>
   );
